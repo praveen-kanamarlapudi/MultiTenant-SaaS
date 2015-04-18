ee278dbc8d08e98baa93e18b092519ee7d3e1174
Praveen Kanamarlapudi <kpraveen420@gmail.com>
2015-04-18 02:14:01 -0700

    Testing commit commands!! :P
    Signed-off-by: Praveen Kanamarlapudi <kpraveen420@gmail.com>

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,17 @@
 /**
  * Made some changes
  */
+
+var db = require("./db");
+
+exports.getData = function(req, res) {
+	var userId = req.param("userId");
+	var mongo = db.mongo([ "waterfall" ]);
+
+	db.find({
+		"userId" : userId
+	}, function(err, result) {
+		
+	});
+
+}
